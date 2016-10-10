bfe7cc2527efe0570270838adfbf873e6f8ccd0f
Daniel Sumba <sumbadaniel1@gmail.com>
2016-10-10 17:07:04 +0300

    Add files via upload
    function to print fizz buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+def fizz_buzz(number):
+	if  (number % 5 == 0 and number % 3 == 0):
+		print "FizzBuzz"
+	elif (number % 5 == 0):
+		print "Buzz"
+	elif (number % 3 == 0):
+		print "Fizz"
+	else
+		print number
+fizz_buzz(3)
